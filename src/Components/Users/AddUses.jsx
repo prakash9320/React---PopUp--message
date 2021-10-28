0bcda6cdfd21029fba02b7177a0a4f27c23e285f
prakash9320 <prakashshukla275@gmail.com>
2021-10-28 16:55:11 +0530

    add lable and input tag

--- src/Components/Users/AddUses.jsx
+++ src/Components/Users/AddUses.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import Card from '../UI/Card';
+import classes from './AddUser.Module.css';
+import Button from '../UI/Button';
 const AddUses = (props) => {
+ const addUserHandler = (event) =>{
+       event.preventDefault();
+
+ }
+
+    
+
+
     return (
-        <form>
+       <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
             <label htmlFor="username">UserName</label>
             <input id="username" type ="text"/>
             <label htmlFor="age">Age(Years)</label>
             <input id="age" type ="number"/>
-            <button type ="submit">Add User</button>
+         <Button type = "onSubmit"> Add User</Button>
         </form>
+       </Card>
     )
 }
 
